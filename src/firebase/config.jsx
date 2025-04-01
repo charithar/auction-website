14ee4c662a8e399f1e7eda16ca7515f3692094c5
Charitha Ratnayaka <charithao@hotmail.com>
2025-04-01 10:29:34 +0530

    Removed config

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -4,12 +4,6 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCVV4uK6dkPZrcNvyyjY2YvinlunumxKYw",
-  authDomain: "silent-action-a8326.firebaseapp.com",
-  projectId: "silent-action-a8326",
-  storageBucket: "silent-action-a8326.firebasestorage.app",
-  messagingSenderId: "465327676502",
-  appId: "1:465327676502:web:1cf9dd4ff5eea6ac6eaff7"
 };
 
 // Initialize Firebase
